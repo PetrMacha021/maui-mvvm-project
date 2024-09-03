d8023245ca6f93d288ab4fd21af77ad197d7f565
Mapetr <info@mapetr.moe>
2024-09-03 11:09:29 +0200

    first commit

---
+++ endproject/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace endproject;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ endproject/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace endproject;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
